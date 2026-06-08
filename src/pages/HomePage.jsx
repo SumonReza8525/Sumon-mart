ce1860ba1702b2456be05bb615b014a5aebf03dd
SumonReza8525 <sumon.esd.bsmrstu@gmail.com>
2026-06-08 15:44:35 +0600

    added our services section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import FeatureSection from "../components/FeatureSection";
 import HeroSection from "../components/HeroSection";
+import Servicex from "../components/Servicex";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection></HeroSection>
       <FeatureSection></FeatureSection>
+      <Servicex></Servicex>
     </div>
   );
 };
